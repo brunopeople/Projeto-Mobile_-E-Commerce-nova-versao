06d278d5d5ad8fed4daa823099da6f80d11c95b7
bruno pessoa <brunopessoa.melo@gmail.com>
2020-07-08 20:05:07 -0300

    Componentes Sobre nós estilizado e criado com sucesso

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Info from "../components/AboutPage/info";
+import Hero from "../components/Hero";
+import aboutBcg from "../images/aboutBcg.jpeg";
 
-export default function AboutPage() {
-  return (
+export default function AboutPage(){
+  return(
     <>
-      <h1>Hello From About Page</h1>
+      <Hero img={aboutBcg}/>
+      <Info/>
     </>
   );
 }
