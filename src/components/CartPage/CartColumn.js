43add861d999c43301234b02efbe57c4a585f17a
bruno pessoa <brunopessoa.melo@gmail.com>
2020-08-26 20:44:14 -0300

    adicionando o total do preço e o layout de carrinho com colunas de preços, quantidade, total e remove.

--- src/components/CartPage/CartColumn.js
+++ src/components/CartPage/CartColumn.js
@@ -1,5 +1,33 @@
 import React from "react";
 
 export default function CartColumns(){
-    return <div>CartColumns</div>;
+    return(
+        <div className="container-fluid text-center d-none d-lg-block my-5">
+          <div className="row">
+              {/** Coluna única */}
+              <div className="col-lg-2">
+                  <p className="text-uppercase">Produtos</p>
+              </div>
+              {/** fim da Coluna única */}
+              
+              {/**Coluna única Produto  */}
+              <div className="col-lg-2">
+                <p className="text-uppercase">Nome do Produto</p>
+              </div>
+              {/**Fim da coluna Produto */}
+
+              {/**Coluna Quantidade */}
+              <div className="col-lg-2">
+                  <p className="text-uppercase">Quantidade</p>
+              </div>
+
+              {/**fim da coluna preço*/}
+              {/**Inicio da coluna de remover*/}
+              <div className="col-lg-2">
+                  <p className="text-uppercase">Total</p>
+              </div>
+              {/**Fim das Colunas */}
+          </div>
+        </div>
+    );
 }
